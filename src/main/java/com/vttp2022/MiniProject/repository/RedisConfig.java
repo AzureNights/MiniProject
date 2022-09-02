ec09cc80a45c8ad3f484fdd54d51efd159bb6062
azurenights <kokilavanielamparidhi@gmail.com>
2022-09-02 20:56:01 +0800

    edited redis config file

--- src/main/java/com/vttp2022/MiniProject/repository/RedisConfig.java
+++ src/main/java/com/vttp2022/MiniProject/repository/RedisConfig.java
@@ -1,6 +1,13 @@
 package com.vttp2022.MiniProject.repository;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -40,6 +47,8 @@ public class RedisConfig {
         return template;
 
     // need to place redis config info 
+
+    //create main file in model and update into mastermind 
 
 
 
