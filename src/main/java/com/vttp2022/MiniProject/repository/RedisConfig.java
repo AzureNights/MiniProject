3613394d3844ff546673247bfcc3f6034b86fb46
azurenights <kokilavanielamparidhi@gmail.com>
2022-09-03 19:28:49 +0800

    Worked on redisconfig and service. Included telegram bot API into service file

--- src/main/java/com/vttp2022/MiniProject/repository/RedisConfig.java
+++ src/main/java/com/vttp2022/MiniProject/repository/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.vttp2022.MiniProject.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,7 @@ import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -17,25 +20,32 @@ public class RedisConfig {
     private String redisHost;
 
     @Value("${spring.redis.port}")
-    private Optional<Integer> redisPort;
+    private Integer redisPort;
+    //why optional 
+    //prev Optional<Integer>
 
     @Value("${spring.redis.password}")
     private String redisPassword;
 
     @Value("${spring.redis.database}")
-    private String redisDatabase;
+    private Integer redisDatabase;
 
     @Bean(name = "games")
     @Scope("singleton")
-    public RedisTemplate<String, Mastermind> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate() {
         final RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
         config.setHostName(redisHost);
-        config.setPort(redisPort.get());
+        config.setPort(redisPort);
+        config.setDatabase(redisDatabase);
         config.setPassword(redisPassword);
+
         Jackson2JsonRedisSerializer jackson2JsonJsonSerializer = new Jackson2JsonRedisSerializer(Mastermind.class);
+        //name the class - from midel ??? 
 
         final JedisClientConfiguration jedisClient = JedisClientConfiguration.builder().build();
         final JedisConnectionFactory jedisFac = new JedisConnectionFactory(config, jedisClient);
+        //why final - comparing with chuks code 
+        
         jedisFac.afterPropertiesSet();
         logger.info("redis host port > {redisHost} {redisPort}", redisHost, redisPort);
         RedisTemplate<String, Mastermind> template = new RedisTemplate<String, Mastermind>();
@@ -45,6 +55,8 @@ public class RedisConfig {
         template.setHashKeySerializer(template.getKeySerializer());
         template.setHashValueSerializer(template.getValueSerializer());
         return template;
+
+    }
 
     // need to place redis config info 
 
--- src/main/java/com/vttp2022/MiniProject/service/ToDoService.java
+++ src/main/java/com/vttp2022/MiniProject/service/ToDoService.java
@@ -1,8 +1,11 @@
 package com.vttp2022.MiniProject.service;
 
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class ToDoService {
@@ -14,13 +17,19 @@ public class ToDoService {
 
     //messaging API or Google sheets API  
      
+    private static String URL = "https://api.telegram.org/bot{token}"
+    //put token in url or not 
 
-    
+    public Optional<what> getMessage (String message){
+        String apiKey = System.getenv("telegramBotAPIKey");
+        String teleURL = UriComponentsBuilder.fromUriString(URL)
+            .queryParam("chat_ID", chatID)
+            .queryParam("text", message)
+            .queryParam("appID", apiKey)
+            .toUriString();
+    }
 
-
-
-
-
+    //logger.info("") WHAT TO INCLUDE 
 
     
 }
